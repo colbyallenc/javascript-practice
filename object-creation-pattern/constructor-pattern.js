6b155bbb32d28ad7d54c01443e2a1a8a0430a0cc
colbyallenc <colbyallenc@gmail.com>
2018-10-07 16:01:18 -0400

    constructor object example notes

--- object-creation-pattern/constructor-pattern.js
+++ object-creation-pattern/constructor-pattern.js
@@ -8,13 +8,15 @@ let carConstructor = function(year, model, color){
     }
 }
 
+// constructor pattern : using the word new 
 let car1 = new carConstructor(2002, 'Acura', 'green');
 let car2 = new carConstructor(2013, 'Infiniti', 'blue');
 
 car1.printCar();
 car2.printCar();
 
-// differs from factory Objects in the way that you dont use a temporary object but you attach and cerate properties for the function(object) itself
+// differs from factory Objects in the way that you dont use a temporary object but you 
+// attach and cerate properties for the function(object) itself
 
 // ________________________________________________________________
 
