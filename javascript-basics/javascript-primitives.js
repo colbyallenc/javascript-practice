7d5c1deb17167903520e6cf3f283e07d41bd9b99
colbyallenc <colbyallenc@gmail.com>
2018-10-08 14:19:11 -0400

    js-fundamentals

---
+++ javascript-basics/javascript-primitives.js
@@ -0,0 +1,47 @@
+// Five primitive Data Types
+
+// 1. Numbers
+// 2. Strings
+// 3. Booleans
+// 4. Null
+// 5. Undefined
+
+
+// Numbers:
+// whole numbers, decimal, and negative numbers
+// javascript doesn't care if the numbers are different. 
+// javascript follows order of operations
+// Modulo: % remainder operation (also called modulus or mod)
+
+
+// Strings:
+// text
+// we can have any character inside of a string
+// single or double quotes
+// concatenation : "charlie" + "brown"
+// escaping characters: start with "\": 
+        // all start with a backslash followed by a double quote
+        // how we tell js that we want a double quote in the string
+        // "she said \"goodbye\" "
+// Strings have length property
+    // "hello".length > 4 | every character counts
+// Access individual characters using [] ans an index
+    // number corresponds with position starting at 0
+    // "hello"[3]
+
+
+// Boolean:
+// either true or false
+
+// Null:
+// these are values
+// only one null
+
+// Undefined:
+// these are values
+// only one undefined
+
+// Numbers and numeric operations
+
+
+// string and common string methods
