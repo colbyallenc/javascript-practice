a874ef970e1496c1b251be53715574de10d67bb0
colbyallenc <colbyallenc@gmail.com>
2018-10-09 11:22:08 -0400

    function problem set

--- javascript-basics/loops/for.js
+++ javascript-basics/loops/for.js
@@ -42,21 +42,21 @@ for(var i= -10; i <= 19; i++){
 
 // 2. print all even numbers between 10 and 40
 for(var i= 10; i<= 40; i++){
-    if(i%2===0){
+    if(i % 2 === 0){
         console.log(i)
     }
 }
 
 // 3. print all odd numbers between 300 and 333
 for(var i= 300; i<= 333; i++){
-    if(i%2!==0){
+    if(i % 2 !== 0){
         console.log(i)
     }
 }
 
 // 4. Print all numbers divisible by both 5 and 3 between 5 and 50
 for(var i= 5; i<= 50; i++){
-    if(i%2===5 && i%2===3){
+    if(i % 2 === 5 && i % 2 === 3){
         console.log(i)
     }
 }
---
+++ javascript-functions/function-declaration vs function expression.js
@@ -0,0 +1,17 @@
+// Function Declaration
+// 
+function capitalize(str){
+    return str.chartAt(0).toUpperCase() + str.slice(1);
+}
+
+// Function Expression
+// your function can become lost is you rename this variable anywhere else in the code
+var capitalization = function(str){
+    return str.chartAt(0).toUpperCase() + str.slice(1);
+}
+
+
+// Functions Problem Set
+
+
+
